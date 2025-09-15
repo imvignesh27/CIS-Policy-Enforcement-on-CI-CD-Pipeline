d1fde308594ac972ec02c136d8348a03525decf7
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-15 13:41:30 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "alb_dns" {
 output "nlb_dns" {
   value = aws_lb.app_nlb.dns_name
 }
+
+output "config_bucket_name" {
+  value = aws_s3_bucket.config_logs.bucket
+}
+
+output "config_recorder_name" {
+  value = aws_config_configuration_recorder.main.name
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.config_role.arn
+}
