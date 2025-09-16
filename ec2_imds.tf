5a5f8ba3e61ce9290109c7f15591317d4665cdd9
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-17 02:35:06 +0530

    Delete ec2_imds.tf

--- ec2_imds.tf
+++
@@ -1,9 +0,0 @@
-# Get all EC2 instances
-data "aws_instances" "all" {}
-
-# Enforce IMDSv2 for each EC2 instance
-resource "aws_instance_metadata_defaults" "imds" {
-  http_tokens                 = "required"
-  http_endpoint               = "enabled"
-  http_put_response_hop_limit = 1
-}
