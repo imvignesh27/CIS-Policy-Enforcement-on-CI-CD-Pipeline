a649f26d1e262fd42bd280e5557a670fe9871823
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-17 12:37:03 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,15 @@
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
 }
 
-output "alb_dns" {
-  value = aws_lb.app_alb.dns_name
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
 }
 
-output "nlb_dns" {
-  value = aws_lb.app_nlb.dns_name
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
 }
 
-output "config_bucket_name" {
-  value = aws_s3_bucket.config_logs.bucket
-}
-
-output "config_recorder_name" {
-  value = aws_config_configuration_recorder.main.name
-}
-
-output "iam_role_arn" {
-  value = aws_iam_role.config_role.arn
-}
-
-output "flow_logs_bucket" {
-  value = aws_s3_bucket.flow_logs.bucket
-}
-
-output "flow_logs_role" {
-  value = aws_iam_role.flow_logs_role.arn
-}
-
-output "enabled_vpc_flow_logs" {
-  value = { for k, v in aws_flow_log.vpc : k => v.id }
+output "nat_gateway_id" {
+  value = aws_nat_gateway.my_nat_gateway.id
 }
